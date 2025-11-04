1b90a6109d7abcf0fee82cfe4367cec6b2eb8819
Abdul Haseeb Butt <abutt20@student.bbk.ac.uk>
2025-11-04 00:51:07 +0000

    add app.js with basic console log

--- PercentagePrinter.java
+++ PercentagePrinter.java
@@ -4,7 +4,7 @@ public class PercentagePrinter {
         // int percentage = (int) (100 * rate);
         // System.out.println(percentage);
         double rate = 8.70;
-        double multiplier = 100;
+        double multiplier = 1000;
         long ans = Math.round((rate * multiplier));
         System.out.println(ans);
     }
